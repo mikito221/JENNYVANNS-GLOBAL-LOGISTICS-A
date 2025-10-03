582af89a886096b3756cab9b13b7d4bf73ffcb00
Uzodimma miracle <uzodimmam784@gmail.com>
2025-10-03 07:35:35 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    alert("👋 Welcome to Jennyvanns Global Logistics!                                       We specialize in freight forwarding, customs clearance, and risk management. Reliable. Transparent. On Time. Every Time.");
+    
+}
+
+ 
